a906d2ed70d00092471178ebe0cb3e3bc57570c3
mshahbazi72 <m.shahbazi72@gmail.com>
2021-03-08 18:43:15 +0100

    update prepare_data.sh

--- prepare_data.sh
+++ prepare_data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python make_hdf5.py --dataset I128 --batch_size 256 --data_root /mnt/efs/fs1/MG/data/
-python calculate_inception_moments.py --dataset I128_hdf5 --data_root /mnt/efs/fs1/MG/data/
+#python make_hdf5.py --dataset I128 --batch_size 256 --data_root /mnt/efs/fs1/MG/data/
+python calculate_inception_moments.py --dataset I128 --data_root /mnt/efs/fs1/MG/data/
 
